25b9a53bc8eabd137964089e5b61c15403f1533a
2022wb86708 <2022wb86708@college.com>
2026-09-25 14:07:25 +0530

    Modify source code for CI test

--- src/main/java/com/wb/App.java
+++ src/main/java/com/wb/App.java
@@ -1,6 +1,6 @@
 package com.wb;
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello Jenkins - 2022wb86708");
+    System.out.println("Hello DevOps - Continuous Integration!");
   }
 }
